8ca7b330ab324480e857ba34084e158fb4a73884
Luis Guilherme <luis.piovan.lg@gmail.com>
2021-05-17 13:05:58 -0300

    implementing methods for data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bytebank';
+  transferencia: any;
+
+  transferir($event: {
+    valor: number | undefined;
+    destino: number | undefined;
+  }) {
+    console.log($event);
+    this.transferencia = $event;
+  }
 }
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent implements OnInit {
+  @Output() aoTransferir = new EventEmitter<any>();
+  @Output() valoresComErro = new EventEmitter<string>();
 
-  constructor() { }
+  transferencia = [];
+  valor: number = 0;
+  destino: number = 0;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  transferir() {
+    console.log('Solicitada nova transferência');
+    if (this.ehValido()) {
+      const valorEmitir = { valor: this.valor, destino: this.destino };
+      this.aoTransferir.emit(valorEmitir);
+    }
   }
 
+  private ehValido() {
+    const valido = this.valor > 0;
+    if (!valido) {
+      this.valoresComErro.emit('Informe um valor válido');
+    }
+    return valido;
+  }
+
+  exibirModalErro(mensagem: any){ }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
 }
